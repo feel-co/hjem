e4bd8c9c4685e88178e2580833b1ace2bd06fac0
éclairevoyant <848000+eclairevoyant@users.noreply.github.com>
2025-08-22 21:37:25 -0400

    support non-symlink types (fixes #49)

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -7,14 +7,15 @@
   utils,
   ...
 }: let
+  inherit (builtins) attrNames attrValues concatLists concatMap concatStringsSep filter mapAttrs toJSON typeOf;
+  inherit (hjem-lib) fileToJson;
   inherit (lib.attrsets) filterAttrs mapAttrsToList;
   inherit (lib.modules) mkIf mkMerge;
   inherit (lib.options) literalExpression mkOption;
   inherit (lib.strings) optionalString;
-  inherit (lib.trivial) pipe;
+  inherit (lib.trivial) flip pipe;
   inherit (lib.types) attrs attrsOf bool either listOf nullOr package raw singleLineStr submoduleWith;
   inherit (lib.meta) getExe;
-  inherit (builtins) filter attrNames attrValues mapAttrs concatLists concatStringsSep typeOf toJSON concatMap;
 
   cfg = config.hjem;
 
@@ -32,46 +33,35 @@
   linker = getExe cfg.linker;
 
   manifests = let
-    mapFiles = files:
-      lib.attrsets.foldlAttrs (
-        accum: _: value:
-          if value.enable -> value.source == null
-          then accum
-          else
-            accum
-            ++ lib.singleton {
-              type = "symlink";
-              inherit (value) source target;
-            }
-      ) []
-      files;
-
     writeManifest = username: let
       name = "manifest-${username}.json";
     in
       pkgs.writeTextFile {
         inherit name;
         destination = "/${name}";
-        text = builtins.toJSON {
-          clobber_by_default = cfg.users."${username}".clobberFiles;
-          version = 1;
-          files = concatMap mapFiles (
-            userFiles cfg.users."${username}"
-          );
+        text = toJSON {
+          version = 2;
+          files = concatMap (
+            flip pipe [
+              attrValues
+              (filter (x: x.enable))
+              (map fileToJson)
+            ]
+          ) (userFiles cfg.users.${username});
         };
         checkPhase = ''
           set -e
           CUE_CACHE_DIR=$(pwd)/.cache
           CUE_CONFIG_DIR=$(pwd)/.config
 
-          ${lib.getExe pkgs.cue} vet -c ${../../manifest/v1.cue} $target
+          ${lib.getExe pkgs.cue} vet -c ${../../manifest/v2.cue} $target
         '';
       };
   in
     pkgs.symlinkJoin
     {
       name = "hjem-manifests";
-      paths = map writeManifest (builtins.attrNames enabledUsers);
+      paths = map writeManifest (attrNames enabledUsers);
     };
 
   hjemModule = submoduleWith {
